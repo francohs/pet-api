249ce8bb9c6f5c4b82dce0f363155e7e2b6bd6c2
Franco <francohormazabal@gmail.com>
2026-02-28 14:35:15 -0500

    fix: add vercel domain to cors

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://pet-web-olive.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 
 void bootstrap();
